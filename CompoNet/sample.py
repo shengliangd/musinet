4b4cc2de62cca2f0bf9e14defac90e04c943a980
Faultrit <faultrit@outlook.com>
2017-05-09 14:55:43 +0800

    changed default parameters in sample.py

--- CompoNet/sample.py
+++ CompoNet/sample.py
@@ -10,27 +10,19 @@ import map_data as nm
 if __name__ == '__main__':
     config = compo_net.Config(training=False,
                               data_path='output/input.pkl')
-
     loader = data_loader.Loader(config)
-
     model = compo_net.Model(config)
-
     sess = tf.Session()
-#    sess.run(tf.variables_initializer(model.variables))  # fail to initialize rnn weights
     sess.run(tf.global_variables_initializer())
-
     model.restore(sess)
 
     inp = loader.get_sequence(pointer=0, length=20)
-
     with open(os.path.join(config.output_dir, 'output.pkl'), 'wb+') as file:
         output = model.sample(sess, inp, final_len=1024)
-        #output = inp
         for i in range(len(output)):
             output[i][0] = nm.unmap_pitch(output[i][0])
             output[i][1] = nm.unmap_dynamic(output[i][1])
             output[i][2] = nm.unmap_rhythm(output[i][2])
             output[i][3] = nm.unmap_duration(output[i][3])
-
         print(output)
         pkl.dump([48, output], file)
