8d509377e115a4fc8268ebdec273a59e7c6c72d6
_rika <ksqsf@mail.ustc.edu.cn>
2017-05-15 21:57:31 +0800

    biosphere: initial

---
+++ Biosphere/biosphere.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+import pickle as pkl
+import numpy as np
+import random
+from ValueNet import value_net, convert
+
+
+class Biosphere:
+    """
+    Genetic Algorithm for Musinet
+    """
+    population = []
+    pop_fitness = []
+    evaluator = value_net.Model()
+    best_fitness = 0.0
+    best = 0
+
+    def __init__(self, initial_file, pop_size=1000, chromlen=512, pm=0.9, pc=0.9):
+        self.pop_size = pop_size
+        self.chromlen = chromlen
+        self.pm = pm
+        self.pc = pc
+        self.load(initial_file)
+        self.rank()
+
+    def load(self, path):
+        """
+        Load a PKL-collection file as the initial population.
+        PKLs are normalized when they're read.
+        """
+        f = open(path, 'rb')
+        for i in range(0, self.pop_size):
+            score = pkl.load(f)
+            notes = score[0][1] # NOTE: will always use the first track
+            for note in notes:
+                note[0] = convert.convert_pitch(note[0])
+                note[1] = convert.convert_dynamic(note[1])
+                note[2] = convert.convert_rhythm(note[2])
+                note[3] = convert.convert_duration(note[3])
+            self.population.append(notes)
+
+    def rank(self):
+        """
+        Rank every individual in the population
+        """
+        self.pop_fitness = self.evaluator.evaluate(self.population)
+
+    def mutate(self):
+        """
+        Genetic mutation on every individual in the population. Probability is @self.pm.
+        Each time only one point in a channel is changed.
+        """
+        def mutate_single(individual):
+            channel = random.randint(0, 3)
+            point = random.randint(0, self.chromlen-1)
+            individual[point][channel] = random.random()
+            return individual
+        mutated_population = []
+        for individual in self.population:
+            if random.random() < self.pm:
+                mutated_population.append(mutate_single(individual))
+            else:
+                mutated_population.append(individual)
+        self.population = mutated_population
+
+    def select(self):
+        """
+        Emulate natural selection using Roulette algorithm.
+        Turn current population into a mating pool.
+        This also maintains best and best_fitness.
+        """
+        def next_i(M):
+            S = 0.0
+            i = 0
+            while S < M and i < self.pop_size:
+                S += self.pop_fitness[i][0]
+                if self.pop_fitness[i][0] > self.best_fitness:
+                    self.best = i
+                    self.best_fitness = self.pop_fitness[i][0]
+                i += 1
+            return i
+        total_fitness = np.sum(self.pop_fitness)
+        selected = []
+        for n in range(0, self.pop_size):
+            M = random.uniform(0, total_fitness)
+            i = next_i(M)
+            if i >= self.pop_size:
+                selected.append(self.population[self.best])
+            else:
+                selected.append(self.population[i])
+        self.population = selected
+
+    def crossover(self):
+        """
+        Cross over using one-point selection. Probability is @self.pc.
+        It exchanges the whole notes (not distinguishing four channels)
+        """
+        def cross_chromosome(A, B):
+            point = random.randint(0, self.chromlen-1)
+            A_ = A[:point] + B[point:]
+            B_ = A[point:] + B[:point]
+            return A_, B_
+        next_gen = []
+        prev = None
+        for individual in self.population:
+            if random.random() < self.pc:
+                if prev is None:
+                    prev = individual
+                else:
+                    next_gen.extend(cross_chromosome(prev, individual))
+                    prev = None
+            else:
+                next_gen.append(individual)
+        self.population = next_gen
+        if prev is not None:
+            self.population.append(prev)
+
+    def describe(self):
+        """
+        Get current average of fitness and the best fitness
+        """
+        return np.mean(self.pop_fitness), self.best_fitness
+
+    def result(self):
+        best = self.population[self.best]
+        print(best)
+        notes = []
+        for note in best:
+            notes.append([convert.deconvert_pitch(note[0]),
+                          convert.deconvert_dynamic(note[1]),
+                          convert.deconvert_rhythm(note[2]),
+                          convert.deconvert_duration(note[3])])
+        part = [48, notes, self.best_fitness]
+        score = [part]
+        return score
---
+++ Biosphere/run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys, os
+from argparse import ArgumentParser
+import pickle as pkl
+musinet_root = '/'.join(os.path.realpath(sys.argv[0]).split('/')[:-2])
+sys.path.append(musinet_root)
+from Biosphere.biosphere import Biosphere
+
+parser = ArgumentParser()
+parser.add_argument('--initial', default=os.path.join(musinet_root, 'data', 'random.pkl'))
+parser.add_argument('--output', default=os.path.join(musinet_root, 'output', 'elite.pkl'))
+args = parser.parse_args()
+
+bio = Biosphere(args.initial)
+epoch = 0
+best_fitness = 0.0
+avg_fitness = 0.0
+try:
+    while avg_fitness < 0.9:
+        avg_fitness, best_fitness = bio.describe()
+        print('epoch %d, avg.=%f, best=%f' % (epoch, avg_fitness, best_fitness))
+        bio.select()
+        bio.mutate()
+        bio.crossover()
+        bio.rank()
+        epoch = epoch + 1
+except KeyboardInterrupt:
+    print('your coffee is finished, right?')
+result = bio.result()
+with open(args.output, 'wb') as f:
+    pkl.dump(result, f)
